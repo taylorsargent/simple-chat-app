78ce376c338c19d34f0f49b9076781fa0ddb1e3a
Eric Adamski <er.adamski@gmail.com>
2016-11-16 13:40:38 -0500

    Adding glup tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglifycss = require('gulp-uglifycss');
+const uglify = require('gulp-uglify');
 const path = require('path');
 const execSync = require('child_process').execSync;
 
 const CSS_DIR = path.resolve('public', 'css');
+const JS_DIR = path.resolve('public', 'js');
 
 gulp.task('compile', () => {
   execSync(`compass clean`, { cwd: __dirname });
@@ -12,10 +14,18 @@ gulp.task('compile', () => {
 });
 
 gulp.task('clean', () =>
-    execSync(`rm -rf ${path.join(__dirname, 'public', 'css', 'style.min.css')}`));
+  execSync(`rm -rf ${path.join(__dirname, 'public', 'css', 'style.min.css')}`));
+
+gulp.task('compress', [ 'compress:css', 'compress:js' ]);
 
 gulp.task('compress:css', [ 'clean', 'compile' ], () =>
   gulp.src(path.join(CSS_DIR, '**/*.css'))
     .pipe(concat('style.min.css'))
     .pipe(uglifycss())
     .pipe(gulp.dest(CSS_DIR)));
+
+gulp.task('compress:js', () =>
+  gulp.src(path.join(JS_DIR, '**/*.js'))
+    .pipe(concat('index.min.js'))
+    .pipe(uglify)
+    .pipe(gulp.dest(JS_DIR)));
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const http          = require('http').Server;
 const utils         = require('./server/utils');
 const server        = http(require('./server'));
 const io            = Io(server);
+const ChatManager   = require('./server/chat');
 
-const ChatManager   = new (require('./server/chat'))(io);
+new ChatManager(io);
 
 // eslint-disable-next-line no-magic-numbers
 const port = utils.normalizePort(process.env.PORT || 3000);
