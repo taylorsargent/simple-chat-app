bf7fbedf62ae8586f66eb693cd72a56a4beb6028
Eric Adamski <er.adamski@gmail.com>
2016-12-20 08:52:25 -0500

    complete

--- lib/components/main.jsx
+++ lib/components/main.jsx
@@ -4,188 +4,202 @@ import io from 'socket.io-client';
 import Utils from './utils';
 import Chat from './utils/chat';
 import Message from './message.jsx';
+import Users from './users.jsx';
 import Notification from './notification.jsx';
 
 class MainScreen extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      notifications: [],
-      messages: [],
-    };
+        this.state = {
+            notifications: [],
+            messages: [],
+            users: [],
+        };
 
-    this.templates = {};
+        this.templates = {};
 
-    Chat.init(this.socket = io());
+        Chat.init(this.socket = io());
 
-    Promise.all([
-      'message',
-      'login',
-      'user',
-      'info-notification',
-      'connect-notification',
-      'disconnect-notification',
-    ].map(template => Utils.getTemplate(this.socket, template)))
-      .then(templates => {
-        templates.map(t => this.templates[t.template] = t.fn);
-        this.updateState({templates: this.templates});
-      });
+        Promise.all([
+            'message',
+            'login',
+            'user',
+            'info-notification',
+            'connect-notification',
+            'disconnect-notification',
+        ].map(template => Utils.getTemplate(this.socket, template)))
+            .then(templates => {
+                templates.map(t => this.templates[t.template] = t.fn);
+                this.updateState({templates: this.templates});
+            });
 
-    this.showLogin = true;
+        this.showLogin = true;
 
-    this.login = Chat.login;
-    this.message = Chat.message;
+        this.login = Chat.login;
+        this.message = Chat.message;
 
-    this.socket.on('username-validation', response => this.onLogin(response));
-    this.socket.on('user-join', name => this.onUserConnect(name));
-    this.socket.on('user-disconnect', name => this.onUserDisconnect(name));
-    this.socket.on('message', response => this.onMessage(response));
-    this.socket.on('messages', response => {
-      const data = JSON.parse(response);
-      Utils.saveUsers(data.users || []);
-      this.updateState({ messages: data.messages });
-      Utils.scrollToBottom();
-    });
-  }
-
-  updateState(state) {
-    this.setState(Object.assign(
-      {},
-      this.state,
-      state
-    ));
-  }
-
-  onMessage(response) {
-    const data = JSON.parse(response);
-    this.state.messages.push(data)
-    this.updateState({ messages: this.state.messages });
-  }
-
-  onLogin(response) {
-    const data = JSON.parse(response);
-    if (data.exists) {
-      this.state.notifications.push({
-        type: 'info',
-        message: `The username ${data.username} is already taken.`,
-      });
-
-      return this.updateState({ notifications: this.state.notifications });
-    } else {
-      Utils.saveUsername(data.username);
-      this.showLogin = false;
-      this.socket.emit('user-join', data.username);
-      this.updateState({ username: data.username });
+        this.socket.on('username-validation', response => this.onLogin(response));
+        this.socket.on('user-join', name => this.onUserConnect(name));
+        this.socket.on('user-disconnect', name => this.onUserDisconnect(name));
+        this.socket.on('message', response => this.onMessage(response));
+        this.socket.on('messages', response => {
+            const data = JSON.parse(response);
+            Utils.saveUsers(data.users || []);
+            this.updateState({ messages: data.messages });
+            Utils.scrollToBottom();
+        });
     }
-  }
 
-  onUserConnect(name) {
-    if (Utils.getUsername() === name) return;
+    updateState(state) {
+        this.setState(Object.assign(
+            {},
+            this.state,
+            state
+        ));
+    }
 
-    const users = Utils.getUsers();
-    users.push(name);
-    Utils.saveUsers(users);
-    this.state.notifications.push({
-      type: 'connect',
-      message: `${name} has connected!`,
-    });
-    this.updateState({ notifications: this.state.notifications });
-  }
+    onMessage(response) {
+        const data = JSON.parse(response);
+        this.state.messages.push(data)
+        this.updateState({ messages: this.state.messages });
+    }
 
-  onUserDisconnect(name) {
-    const users = Utils.getUsers();
-    users.splice(users.indexOf(name), 1);
-    Utils.saveUsers(users);
-    this.state.notifications.push({
-      type: 'disconnect',
-      username: `${name} has disconnected!`,
-    });
-    this.updateState({ notifications: this.state.notifications });
-  }
+    onLogin(response) {
+        const data = JSON.parse(response);
+        if (data.exists) {
+            this.state.notifications.push({
+                type: 'info',
+                message: `The username ${data.username} is already taken.`,
+            });
 
-  render() {
-    const { messages, notifications } = this.state;
-
-    return (
-      <main className="chat-app">
-        <div
-            className={[ 'login', this.showLogin ? 'show' : 'hide' ].join(' ')}
-        >
-          <form className="login-form">
-            <div
-              dangerouslySetInnerHTML={{__html: this.templates.login ? this.templates.login() : '' }}
-            />
-            <button
-              className="login-button"
-              onClick={e => {
-                e.preventDefault();
-                this.login(document.getElementById('username').value);
-
-                return false;
-              }}
-            >
-              Login
-            </button>
-          </form>
-        </div>
-        <div className="notification-container">
-          <ul className="notifications">
-          {
-            notifications.map((notification, index) => {
-              setTimeout(() => {
-                notifications.splice(index, 1);
-                this.updateState({ notifications: notifications });
-            }, 20000);
-
-              return (
-                <Notification
-                  key={index}
-                  template={this.templates[`${notification.type}-notification`]}
-                  {...notification}
-                />
-              );
-            })
-          }
-          </ul>
-        </div>
-        <ul className="messages">
-        {
-          messages.map((message, index) => {
-            if (!this.templates.message) return null;
-
-            return (
-              <Message
-                key={index}
-                template={this.templates.message}
-                {...message}
-              />
-            );
-          })
+            return this.updateState({ notifications: this.state.notifications });
+        } else {
+            Utils.saveUsername(data.username);
+            this.showLogin = false;
+            this.socket.emit('user-join', data.username);
+            this.updateState({ username: data.username });
         }
-        </ul>
-        <form
-            id="message-bay"
-            onSubmit={e => {
-              e.preventDefault();
-              this.message(this.refs.m.value);
-              this.refs.m.value = '';
-            }}
-        >
-          <textarea
-            className="message-container"
-            ref="m"
-          />
-          <button
-            type="submit"
-            className="message-button"
-          >
-            Send
-          </button>
-        </form>
-      </main>
-    );
-  }
+    }
+
+    onUserConnect(name) {
+        if (Utils.getUsername() === name) return;
+
+        const users = Utils.getUsers();
+        users.push(name);
+        Utils.saveUsers(users);
+        this.state.notifications.push({
+            type: 'connect',
+            message: `${name} has connected!`,
+        });
+        this.updateState({
+            notifications: this.state.notifications,
+            users: users,
+        });
+    }
+
+    onUserDisconnect(name) {
+        const users = Utils.getUsers();
+        users.splice(users.indexOf(name), 1);
+        Utils.saveUsers(users);
+        this.state.notifications.push({
+            type: 'disconnect',
+            username: `${name} has disconnected!`,
+        });
+        this.updateState({
+            notifications: this.state.notifications,
+            users: users,
+        });
+    }
+
+    render() {
+        const { messages, notifications, users } = this.state;
+
+        return (
+          <main className="chat-app">
+            <div
+                className={[ 'login', this.showLogin ? 'show' : 'hide' ].join(' ')}
+            >
+              <form className="login-form">
+                <div
+                  dangerouslySetInnerHTML={{__html: this.templates.login ? this.templates.login() : '' }}
+                />
+                <button
+                  className="login-button"
+                  onClick={e => {
+                      e.preventDefault();
+                      this.login(document.getElementById('username').value);
+
+                      return false;
+                  }}
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+            <div className="notification-container">
+              <ul className="notifications">
+              {
+                notifications.map((notification, index) => {
+                    setTimeout(() => {
+                        notifications.splice(index, 1);
+                        this.updateState({ notifications: notifications });
+                    }, 2000);
+
+                    return (
+                      <Notification
+                        key={index}
+                        template={this.templates[`${notification.type}-notification`]}
+                        {...notification}
+                      />
+                    );
+                })
+              }
+              </ul>
+            </div>
+            <div className="user-list">
+                <Users
+                    users={users}
+                    template={this.templates.user}
+                />
+            </div>
+            <ul className="messages">
+            {
+              messages.map((message, index) => {
+                  if (!this.templates.message) return null;
+
+                  return (
+                      <Message
+                          key={index}
+                          template={this.templates.message}
+                          {...message}
+                      />
+                  );
+              })
+            }
+            </ul>
+            <form
+                id="message-bay"
+                onSubmit={e => {
+                    e.preventDefault();
+                    this.message(this.refs.m.value);
+                    this.refs.m.value = '';
+                }}
+            >
+              <textarea
+                className="message-container"
+                ref="m"
+              />
+              <button
+                type="submit"
+                className="message-button"
+              >
+                Send
+              </button>
+            </form>
+          </main>
+        );
+    }
 }
 
 export default MainScreen;
--- lib/components/message.jsx
+++ lib/components/message.jsx
@@ -2,28 +2,28 @@ import React, {PropTypes} from 'react';
 import moment from 'moment';
 
 class Message extends React.PureComponent {
-  render() {
-    const { template, username, message, timestamp } = this.props;
+    render() {
+        const { template, username, message, timestamp } = this.props;
 
-    return (
-      <li
-          dangerouslySetInnerHTML={{
-            __html: template({
-              username,
-              message,
-              timestamp: moment(timestamp).format('HH:MM:SS'),
-            }),
-          }}
-      />
-    );
-  }
+        return (
+            <li
+                dangerouslySetInnerHTML={{
+                    __html: template({
+                        username,
+                        message,
+                        timestamp: moment(timestamp).format('HH:MM:SS'),
+                    }),
+                }}
+            />
+        );
+    }
 }
 
 Message.propTypes = {
-  template: PropTypes.func.isRequired,
-  message: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  timestamp: PropTypes.number,
+    template: PropTypes.func.isRequired,
+    message: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    timestamp: PropTypes.number,
 };
 
 export default Message;
