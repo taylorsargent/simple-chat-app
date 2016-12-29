1f11cf14000ade3b676f317069341974040d0cd5
Eric Adamski <er.adamski@gmail.com>
2016-12-29 12:55:53 -0500

    cloning complete

--- public/js/user-menu.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-'use strict';
-
-$(function () {
-    $('.user-list-button').on('touchstart click', function (e) {
-        $('.user-list').toggleClass('open');
-        $('.user-list-button').toggleClass('active');
-    });
-});
